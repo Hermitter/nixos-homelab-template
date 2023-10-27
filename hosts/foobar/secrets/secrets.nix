dddabcb4d456ee663c03c01d8f14544484f59e03
Carlos Chacin <Hermitter@jellyapple.com>
2023-10-26 20:31:45 -0400

    Fix typo

--- hosts/foobar/secrets/secrets.nix
+++ hosts/foobar/secrets/secrets.nix
@@ -1,7 +1,7 @@
 # Reminders: 
 # - This file is only read while using the agenix CLI.
-# - Changing any keys will require you to re-encry.
-# - Secrets should only be referenced by paths and not value in your configurations, unless you want it exposed in the nix store.
+# - Changing any keys will require you to rekey.
+# - Be careful to only reference a secret by it's path in your configuration, unless you want it exposed in the nix store.
 let
   hosts = import ../../default.nix;
   globals = import ../../../globals.nix;
